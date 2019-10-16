2b239980edcae51ad95717175a8291b0d05d1856
Marialbert Flores <marialbert1997@gmail.com>
2019-10-16 09:58:31 -0400

    Editar perfil

--- src/router.js
+++ src/router.js
@@ -1,67 +1,73 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
-import Ver_perfil from './views/Ver_perfil'
-import Acerca_de from './views/Acercade'
-import Sugerencia from './views/Sugerencia'
-import Que_hay_denuevo from './views/Nuevo'
-import Ajustes from './views/Ajustes'
-import New_post from './views/New_post'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+import Ver_perfil from "./views/Ver_perfil";
+import Acerca_de from "./views/Acercade";
+import Sugerencia from "./views/Sugerencia";
+import Que_hay_denuevo from "./views/Nuevo";
+import Ajustes from "./views/Ajustes";
+import New_post from "./views/New_post";
+import Editar_perfil from "./views/Editar_perfil";
 
-
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: Home
     },
     {
-      path: '/Ver_perfil',
-      name: 'Ver_perfil',
+      path: "/Ver_perfil",
+      name: "Ver_perfil",
       component: Ver_perfil
     },
     {
-      path: '/Acercade',
-      name: 'Acercade',
+      path: "/Acercade",
+      name: "Acercade",
       component: Acerca_de
     },
     {
-      path: '/Sugerencia',
-      name: 'Sugerencia',
+      path: "/Sugerencia",
+      name: "Sugerencia",
       component: Sugerencia
     },
     {
-      path: '/Que_hay_denuevo',
-      name: 'Que_hay_denuevo',
+      path: "/Que_hay_denuevo",
+      name: "Que_hay_denuevo",
       component: Que_hay_denuevo
     },
     {
-      path: '/Ajustes',
-      name: 'Ajustes',
+      path: "/Ajustes",
+      name: "Ajustes",
       component: Ajustes
     },
     {
-      path: '/Acerca_de',
-      name: 'Acerca_de',
+      path: "/Acerca_de",
+      name: "Acerca_de",
       component: Acerca_de
-    },  
+    },
     {
-      path: '/New_post',
-      name: 'New_post',
+      path: "/New_post",
+      name: "New_post",
       component: New_post
-    },  
+    },
     {
-      path: '/about',
-      name: 'about',
+      path: "/Editar_perfil",
+      name: "Editar_perfil",
+      component: Editar_perfil
+    },
+    {
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/About.vue")
     }
   ]
-})
+});
---
+++ src/views/Editar_perfil.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-container grid-list-lg>
+    <v-app-bar app dark flat hide-on-scroll>
+      <v-btn icon :to="{name: 'Editar_perfil'}">
+        <v-icon>arrow_back</v-icon>
+      </v-btn>
+      <v-toolbar-title>Editar Perfil</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-img aspect-ratio="1" max-width="50" max-height="40" src="../assets/act.png"></v-img>
+      <v-btn icon>
+        <v-icon>done</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-flex xs12>
+      <!-- <v-card class="ml-4 mr-4" dense>-->
+      <v-form class="px-3">
+        <!-- Mostrar foto de perfil y boton cambiar-->
+        <div class="text-center">
+          <v-flex xs12 class="text-xs-center text-sm-center text-md-center text-lg-center">
+            <v-avatar size="80">
+              <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="avatar" />
+            </v-avatar>
+          </v-flex>
+          <v-btn class="ma-2" outlined color="#69a286">Cambiar foto de perfil</v-btn>
+        </div>
+
+        <!-- Datos Personales -->
+        <v-text-field color="oficial" label=" " placeholder="Nombre"></v-text-field>
+        <v-text-field color="oficial" label=" " placeholder="Nombre de usuario"></v-text-field>
+        <v-text-field color="oficial" label=" " placeholder="Correo"></v-text-field>
+
+        <v-text-field color="oficial" label=" " placeholder="www.example.com" persistent-hint></v-text-field>
+        <v-textarea color="oficial" label=" " placeholder="Informaión de interés"></v-textarea>
+
+        <!-- Sexo-->
+
+        <v-flex xs12>
+          <v-combobox v-model="select" :items="items" label="Selecciona tu sexo" color="oficial"></v-combobox>
+        </v-flex>
+      </v-form>
+      <!--   </v-card> -->
+    </v-flex>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      select: "Mujer",
+      items: ["Mujer", "Hombre"]
+    };
+  }
+};
+</script>
